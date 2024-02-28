a1c52e4a00488a697809268a622065b76a41bec7
1996Aurel <121833780+1996Aurel@users.noreply.github.com>
2024-02-28 11:02:53 +1100

    Create slurm.sh

---
+++ slurm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32G
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+
+source /scratch/cai/uqaamar/anaconda/3.7/bin/activate /scratch/cai/uqaamar/demo_env
+python main.py
